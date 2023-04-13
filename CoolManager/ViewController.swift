7fdfe8e64a3fbb3c3fa23ada8fc49bdfc5c00239
antonio cortes <antonio.cortes@neoris.com.mx>
2023-04-12 21:23:08 -0600

    now i know how to show differents screens with a vertical menu

--- CoolManager/ViewController.swift
+++ CoolManager/ViewController.swift
@@ -14,37 +14,37 @@ class ViewController: UIViewController {
     
     @IBAction func btn_1() {
         let view:view1 = view1.fromNib()
-        content = view
+        content.addSubview(view)
     }
     
     @IBAction func btn_2() {
         let view:view2 = view2.fromNib()
-        content = view
+        content.addSubview(view)
     }
     
     @IBAction func btn_3() {
-        
+        let view:view3 = view3.fromNib()
+        content.addSubview(view)
     }
     
     @IBAction func btn_4() {
-        
+        let view:view4 = view4.fromNib()
+        content.addSubview(view)
     }
     
     @IBAction func btn_5() {
-        
+        let view:view5 = view5.fromNib()
+        content.addSubview(view)
     }
     
     @IBAction func btn_6() {
-        
+        let view:view6 = view6.fromNib()
+        content.addSubview(view)
     }
     
     @IBAction func btn_7() {
-        
+        let view:view7 = view7.fromNib()
+        content.addSubview(view)
     }
-    
-    @IBAction func btn_8() {
-    }
-    
-    
 }
 
