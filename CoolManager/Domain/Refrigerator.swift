b08570403764ebc72d08d21421bab0fb28fd20fe
Antonio Cortes <antoni_anemi@hotmail.com>
2023-06-23 21:27:36 -0700

    Remove states from the project for now.

--- CoolManager/Domain/Refrigerator.swift
+++ CoolManager/Domain/Refrigerator.swift
@@ -1,7 +1,6 @@
 import Foundation
 public class Refrigerator {
     private(set) var setPoint: SetPoint
-    private var state: RefrigeratorState?
     private(set) var components = [RefrigeratorComponent]()
     private var stateName:String
     init(_ stateName:String="",
@@ -67,10 +66,7 @@ public class Refrigerator {
     }
     
     func currentStateName() -> String {
-            guard let currentState = state else {
                 return "No state"
-            }
-            return currentState.name
         }
     
     
@@ -83,63 +79,3 @@ public class Refrigerator {
     }
 }
 
-extension Refrigerator {
-    enum StateEnum {
-            case normal
-            case eco
-            case night
-            case smart
-            case lowpower
-            case turbo
-            case standby
-            case holiday
-            case storage
-            case childLock
-            case freshProduce
-            case quickCooling
-            case maintenance
-            case energySaver
-            case defrost
-            case party
-            case freezing
-    }
-        
-    func setState(_ state: StateEnum) {
-            switch state {
-            case .normal:
-                self.state = NormalState(refrigerator: self)
-            case .eco:
-                self.state = EcoState(refrigerator: self)
-            case .childLock:
-                self.state = ChildLockState(refrigerator: self)
-            case .smart:
-                self.state = SmartModeState(refrigerator: self)
-            case .night:
-                self.state = NightModeState(refrigerator: self)
-            case .lowpower:
-                self.state = LowPowerState(refrigerator: self)
-            case .turbo:
-                self.state = TurboState(refrigerator: self)
-            case .standby:
-                self.state = StandbyState(refrigerator: self)
-            case .holiday:
-                self.state = HolidayModeState(refrigerator: self)
-            case .storage:
-                self.state = StorageState(refrigerator: self)
-            case .freshProduce:
-                self.state = FreshProduceState(refrigerator: self)
-            case .quickCooling:
-                self.state = QuickCoolingState(refrigerator: self)
-            case .maintenance:
-                self.state = MaintenanceModeState(refrigerator: self)
-            case .energySaver:
-                self.state = EnergySaverState(refrigerator: self)
-            case .defrost:
-                self.state = DefrostState(refrigerator: self)
-            case .party:
-                self.state = PartyState(refrigerator: self)
-            case .freezing:
-                self.state = FreezingState(refrigerator: self)
-            }
-        }
-}
