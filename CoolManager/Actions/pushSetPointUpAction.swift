c2a56afc745d230c1568702f4b4584eed650ee5b
Antonio Cortes <antoni_anemi@hotmail.com>
2023-06-25 00:52:00 -0700

    Updates method

--- CoolManager/Actions/pushSetPointUpAction.swift
+++ CoolManager/Actions/pushSetPointUpAction.swift
@@ -1,5 +1,5 @@
 final class pushSetPointUpAction:Action {
     var setPoint:SetPoint
     init(_ setPoint:SetPoint){ self.setPoint = setPoint }
-    func excecute() { self.setPoint.upScaleSetPoint() }
+    func excecute() { self.setPoint.up() }
 }
